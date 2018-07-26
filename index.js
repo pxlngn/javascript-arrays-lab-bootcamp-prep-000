723b7f5b6ce938af99d8dc4ef8ca634cd8bc6e58
jthomps86@gmail.com <jthomps86@gmail.com>
2018-07-26 23:14:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,10 +20,15 @@ function destructivelyRemoveFirstKitten() {
 function appendKitten(name) {
   return [...kittens, name]
 }
+
 function prependKitten(name) {
   return [name, ...kittens]
 }
 function removeLastKitten() {
   return kittens.slice(0,kittens.length -1)
 }
+function removeFirstKitten() {
+  return kittens.slice(0,1)
+}
+
 
