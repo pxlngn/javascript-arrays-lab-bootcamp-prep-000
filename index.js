8b510e87e46ceea648417a2c9c4b501fd6b4fb29
jthomps86@gmail.com <jthomps86@gmail.com>
2018-07-26 23:12:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,6 +24,6 @@ function prependKitten(name) {
   return [name, ...kittens]
 }
 function removeLastKitten() {
-  return kittens.slice(1,kittens.length -1)
+  return kittens.slice(0,kittens.length -1)
 }
 
